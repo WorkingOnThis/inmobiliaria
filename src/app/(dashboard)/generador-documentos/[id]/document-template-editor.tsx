cd079c972b7705f536a479d391835305c831c8b4
Gaston <arce.guillermo.gaston@email.com>
2026-04-23 01:08:39 -0300

    feat(document-templates): print formatting

--- src/app/(dashboard)/generador-documentos/[id]/document-template-editor.tsx
+++ src/app/(dashboard)/generador-documentos/[id]/document-template-editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "./print.css";
 import { useState, useRef, useCallback } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
